8c4be90ab6f4213301491926dc70f742c10997c8
Shrekie <thomaslindauer@mail.com>
2017-07-06 09:03:55 +0200

    config change

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,8 @@ var env = process.env.NODE_ENV || 'development';
 
 if(env === 'development'){
 	process.env.PORT = 3000;
-	process.env.dbURI = 'mongodb://localhost:27017/PictureSaveApp';
+	process.env.dbURI = 'mongodb://localhost:27017/UserApp';
 }else if(env === 'test'){
 	process.env.PORT = 3000;
-	process.env.dbURI = 'mongodb://localhost:27017/PictureSaveAppTest';
+	process.env.dbURI = 'mongodb://localhost:27017/UserApp';
 }
